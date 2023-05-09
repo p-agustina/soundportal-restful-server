0ea07db873a6ac3fa3ce168f09a9d24ba00b6ee3
AGUSTINA PATT <agustinapatt@gmail.com>
2023-05-09 14:36:06 +0100

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("soundportal-restful-server JS imported successfully!");
+});
